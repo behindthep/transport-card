9b0be945877f2691291debc43332dda1856b6ddb
Alexey <183180049+behindthep@users.noreply.github.com>
2025-11-19 10:14:23 +0300

    add InputBalanceValidator class

---
+++ src/main/java/github/behindthep/InputBalanceValidator.java
@@ -0,0 +1,20 @@
+package github.behindthep;
+
+public class InputBalanceValidator {
+    public static int validateBalance(Scanner input) {
+        System.out.println("Enter balance: ");
+        int balance = -1;
+        while (balance < 0) {
+            if (input.hasNextInt()) {
+                balance = input.nextInt();
+                if (balance < 0) {
+                    System.out.println("Balance should be positive.");
+                }
+            } else {
+                System.out.println("Balance should be a number.");
+                input.next();
+            }
+        }
+        return balance;
+    }
+}
