11f3fe4fbf4a9f7510505db432772f15b65ec09e
euphausia superba <183180049+behindthep@users.noreply.github.com>
2025-11-29 12:42:35 +0300

    trip to IntelliJ IDEA
    # Conflicts:
    #	src/main/java/github/behindthep/InputBalanceValidator.java
    
    diff --git a/.gitignore b/.gitignore
    new file mode 100644
    index 0000000..723ef36
    --- /dev/null
    +++ b/.gitignore
    @@ -0,0 +1 @@
    +.idea
    \ No newline at end of file
    diff --git a/src/main/java/github/behindthep/AvailableTripDaysCalculator.java b/src/com/github/behindthep/AvailableTripDaysCalculator.java
    similarity index 62%
    rename from src/main/java/github/behindthep/AvailableTripDaysCalculator.java
    rename to src/com/github/behindthep/AvailableTripDaysCalculator.java
    index 1547dc3..0032c2e 100644
    --- a/src/main/java/github/behindthep/AvailableTripDaysCalculator.java
    +++ b/src/com/github/behindthep/AvailableTripDaysCalculator.java
    @@ -1,12 +1,13 @@
    -package github.behindthep;
    +//
    +package com.github.behindthep;
    
    +//
     import java.time.DayOfWeek;
     import java.time.LocalDate;
     import java.time.format.DateTimeFormatter;
     import java.util.Scanner;
     import github.behindthep.InputBalanceValidator;
    
    -// TODO: Как правильно писать консольную утилиту
     public class AvailableTripDaysCalculator {
         private static final byte TRIP_COST_PER_DAY = 34 * 2;
    
    @@ -23,6 +24,23 @@ public class AvailableTripDaysCalculator {
             return "Top up card before: " + dateOfReplenishment.format(DateTimeFormatter.ofPattern("d MMMM yyyy"));
         }
    
    +    public static int validateBalance(Scanner input) {
    +        System.out.println("Enter balance: ");
    +        int balance = -1;
    +        while (balance < 0) {
    +            if (input.hasNextInt()) {
    +                balance = input.nextInt(); // nextInt() игнорирует пробелы
    +                if (balance < 0) {
    +                    System.out.println("Balance should be positive.");
    +                }
    +            } else {
    +                System.out.println("Balance should be a number.");
    +                input.next(); // съедаем некорректный ввод
    +            }
    +        }
    +        return balance;
    +    }
    +
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
             System.out.println(calculateAvailableTripDays(InputBalanceValidator.validateBalance(input)));
    diff --git a/src/main/java/github/behindthep/InputBalanceValidator.java b/src/main/java/github/behindthep/InputBalanceValidator.java
    deleted file mode 100644
    index 5961a54..0000000
    --- a/src/main/java/github/behindthep/InputBalanceValidator.java
    +++ /dev/null
    @@ -1,20 +0,0 @@
    -package github.behindthep;
    -
    -public class InputBalanceValidator {
    -    public static int validateBalance(Scanner input) {
    -        System.out.println("Enter balance: ");
    -        int balance = -1;
    -        while (balance < 0) {
    -            if (input.hasNextInt()) {
    -                balance = input.nextInt(); // nextInt() игнорирует пробелы
    -                if (balance < 0) {
    -                    System.out.println("Balance should be positive.");
    -                }
    -            } else {
    -                System.out.println("Balance should be a number.");
    -                input.next(); // съедаем некорректный ввод
    -            }
    -        }
    -        return balance;
    -    }
    -}

--- src/com/github/behindthep/AvailableTripDaysCalculator.java
+++ src/com/github/behindthep/AvailableTripDaysCalculator.java
@@ -1,12 +1,13 @@
-package github.behindthep;
+//
+package com.github.behindthep;
 
+//
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import github.behindthep.InputBalanceValidator;
 
-// TODO: Как правильно писать консольную утилиту
 public class AvailableTripDaysCalculator {
     private static final byte TRIP_COST_PER_DAY = 34 * 2;
 
@@ -21,6 +22,23 @@ public class AvailableTripDaysCalculator {
             }
         }
         return "Top up card before: " + dateOfReplenishment.format(DateTimeFormatter.ofPattern("d MMMM yyyy"));
+    }
+
+    public static int validateBalance(Scanner input) {
+        System.out.println("Enter balance: ");
+        int balance = -1;
+        while (balance < 0) {
+            if (input.hasNextInt()) {
+                balance = input.nextInt(); // nextInt() игнорирует пробелы
+                if (balance < 0) {
+                    System.out.println("Balance should be positive.");
+                }
+            } else {
+                System.out.println("Balance should be a number.");
+                input.next(); // съедаем некорректный ввод
+            }
+        }
+        return balance;
     }
 
     public static void main(String[] args) {
